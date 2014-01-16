ba72c517018622c0f9f1e1793e85e4395750afc4
Masato KAMINOKURA <mstk.by.knife@gmail.com>
2014-01-17 00:55:20 +0900

    modified.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,19 +12,16 @@ public class Application extends Controller {
     static Form<Task> taskForm = Form.form(Task.class);
 
     public static Result index() {
-        //return ok(index.render("Your new application is ready."));
         return redirect(routes.Application.tasks());
     }
 
     public static Result tasks() {
-        //return TODO;
         return ok(
             index.render(Task.all(), taskForm)
         );
     }
 
     public static Result newTask() {
-        //return TODO;
         Form<Task> fieldForm = taskForm.bindFromRequest();
         if(fieldForm.hasErrors()) {
             return badRequest(
